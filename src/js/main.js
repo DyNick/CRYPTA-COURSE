93ffb3ff16a39b8ce7b3cb887f452a8f26c71e04
DyNick <kokapunx@gmail.com>
2017-12-20 14:16:26 +0200

    marge-version

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
 
 
+// header fixed
+$(window).scroll( function() {
+    var header = $('.header');
+    if( $(window).scrollTop() >= 100 ){
+        header.addClass('header--active');
+    } else {
+        header.removeClass('header--active');
+    }
+});
+// SLIDER SPEAKERS REVIEWS
+$(document).ready(function() {
+    var sliderReviews = $('.reviews_slider');
+    sliderReviews.owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
+
+
 
 
