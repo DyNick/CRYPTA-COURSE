c9a48e116bed339b94cb7753701d64e56957c8a4
DyNick <kokapunx@gmail.com>
2017-12-21 13:44:00 +0200

    video-popup with video content

--- src/js/main-page/exspert-section.js
+++ src/js/main-page/exspert-section.js
@@ -1,4 +1,36 @@
 $(document).ready(function (){
+/*    var id = '';
+    $('.item-image--expert-section').on('click', function (e) {
+
+        var popupId = $(this).attr("data-id");
+        console.log(popupId);
+        console.log(".video-popup #" + popupId);
+         $(".video-popup #" + popupId).addClass('_visible');
+
+
+
+
+
+
+    });*/
+    $(function() {
+        //----- OPEN
+        $('[data-popup-open]').on('click', function(e)  {
+            var targeted_popup_class = jQuery(this).attr('data-popup-open');
+            $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+            e.preventDefault();
+        });
+
+        //----- CLOSE
+        $('[data-popup-close]').on('click', function(e)  {
+            var targeted_popup_class = jQuery(this).attr('data-popup-close');
+            $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+            e.preventDefault();
+        });
+    });
+
     var owlExperts = $('.owl-carousel');
     owlExperts.owlCarousel({
         loop:true,
